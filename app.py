9ca2ec8268bf5bfb28de476fba5c583d86d4c14a
Maltonn <ymatsubayashi09@outlook.jp>
2023-06-22 20:51:29 +0900

    update

--- app.py
+++ app.py
@@ -7,10 +7,15 @@ app = Flask(__name__)
 CORS(app)
 
 
+@app.route("/", methods=["GET"])
+def main():
+    status_code=200
+    return jsonify({"res":"hello!!"}),status_code
+
 @app.route("/api", methods=["GET"])
 def main():
     url = request.args.get("url")
-    dic,status_code=main(url)
+    dic,status_code=main(url,save_pickle=True,save_firestore=False)
     return jsonify(dic),status_code
     
 if __name__ == "__main__":
